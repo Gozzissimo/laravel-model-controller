531dda79ca1eeb232e6060aaa50199208bf7e18e
Gozzissimo <fantini.nico@gmail.com>
2022-02-18 00:14:12 +0100

    pritn db on homepage

--- app/Http/Controllers/PageController.php
+++ app/Http/Controllers/PageController.php
@@ -10,9 +10,9 @@ class PageController extends Controller
 {
     public function index() {
         $movies = Movie::all();
-        dd($movies);
+        // dd($movies);
 
-        return view('home');
+        return view('home', ['movies' => $movies]);
     }
 
 }
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,5 +1,13 @@
 @extends('layouts.base')
 
 @section('content')
-    Home
+<ul>
+    @foreach ($movies as $movie)
+    <li>
+        <h2>{{ $movie['title'] }}</h2>
+        <h3>{{ $movie['original_title'] }}</h3>
+        <div>{{ $movie['vote'] }}</div>
+    </li>
+    @endforeach
+</ul>
 @endsection
