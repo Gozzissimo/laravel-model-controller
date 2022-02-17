31d1c65189b318109477c56fea18db5d348d6926
Gozzissimo <fantini.nico@gmail.com>
2022-02-17 18:42:46 +0100

    fixed php.ini settings

--- app/Http/Controllers/PageController.php
+++ app/Http/Controllers/PageController.php
@@ -9,9 +9,9 @@ use App\Movie;
 class PageController extends Controller
 {
     public function index() {
-        
         $movies = Movie::all();
         dd($movies);
+
         return view('home');
     }
 
